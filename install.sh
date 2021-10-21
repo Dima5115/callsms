039474d0fe968f61ebbdff97ed2a835214f570f7
Dima <91055603+Dima5115@users.noreply.github.com>
2021-10-21 16:47:08 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+pkg update
+pkg upgrade
+pkg install git
+pkg install pip
+pkg install python
+pkg install requests
+pkg install figlet
+pkg install python2
+pkg install pip2
+pkg install ruby
+pkg install nano
